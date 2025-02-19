e3232bd8744b7eab16e275a095f99a377b533fb2
PrinceDobariya0710 <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-02-19 23:24:58 +0530

    react working

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 
 function App() {
-
+  const tt = "variabke"
   return (
-    <h1>
-      Chai aur Code with vite
-    </h1>
+    <>
+      <h1>
+        Chai aur Code with vite {tt}
+      </h1>
+    </>
+
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// const anotherUser = "ChaiAurReact"
+// const reactElement = React.createElement(
+//   'a',
+//   { href: 'https://google.com', target: '_blank' },
+//   'click me to visit google ',
+
+// )
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
