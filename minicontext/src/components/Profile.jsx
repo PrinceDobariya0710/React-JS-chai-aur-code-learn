d19c6edb0c6d99701cebfa8ef03e5da0502c9122
PrinceDobariya0710 <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-02-28 12:28:46 +0530

    ContextAPI projects

---
+++ minicontext/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import UserContext from '../context/UserContext'
+
+const Profile = () => {
+    const { user } = useContext(UserContext)
+    console.log(user)
+    if (!user) return <div>Please Login</div>
+
+    return <div>Welcome {user}</div>
+
+
+}
+
+export default Profile
